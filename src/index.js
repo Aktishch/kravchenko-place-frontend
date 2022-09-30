38efcacf67baf758cf17e26b89eff772eb59ddfe
Александр Актищев <aktishch@gmail.com>
2022-09-30 10:09:08 +0300

    remove files

--- src/index.js
+++ src/index.js
@@ -6,31 +6,17 @@ import airDatepicker from './lib/air-datepicker'
 
 // Scripts
 import anchorTransition from './ts/data-href'
-import formatImg from './ts/format-img'
 import listing from './ts/listing'
 import scrolled from './ts/scrolled'
 import waved from './ts/waved'
 import lightening from './ts/lightening'
 import maskTel from './ts/mask-tel'
 import inputs from './ts/inputs'
-import range from './ts/range'
-import password from './ts/password'
 import quantity from './ts/quantity'
 import formValidate from './ts/form-validate'
-import formData from './ts/form-data'
-import writeText from './ts/write-text'
-import outNumber from './ts/out-number'
-import choiceFile from './ts/choice-file'
 import scrollHeader from './ts/scroll-header'
 import mobileMenu from './ts/mobile-menu'
-import smartMenu from './ts/smart-menu'
 import draggableBtn from './ts/draggable-btn'
-import accordion from './ts/accordion'
-import filter from './ts/filter'
-import paralax from './ts/paralax'
-import horizontalScrolling from './ts/horizontal-scrolling'
-import timeCounter from './ts/time-counter'
-import player from './ts/player'
 import theme from './ts/theme'
 
 // Style
@@ -44,31 +30,17 @@ window.addEventListener('DOMContentLoaded', () => {
     sliderSwiper.init()
     airDatepicker.init()
     anchorTransition.init()
-    // formatImg.init()
     listing.init()
     scrolled.init()
     waved.init()
     lightening.init()
     maskTel.init()
     inputs.init()
-    // range.init()
-    // password.init()
     quantity.init()
     formValidate.init()
-    // formData.init()
-    // writeText.init()
-    // outNumber.init()
-    // choiceFile.init()
     scrollHeader.init()
     mobileMenu.init()
-    // smartMenu.init()
     draggableBtn.init()
-    // accordion.init()
-    // filter.init()
-    // paralax.init()
-    // horizontalScrolling.init()
-    // timeCounter.init()
-    // player.init()
     theme.init()
     
 })
