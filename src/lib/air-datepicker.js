85e94f7936579b157c4a306671e82d568a7171db
Александр Актищев <aktishch@gmail.com>
2022-11-02 16:39:50 +0300

    calendar

--- src/lib/air-datepicker.js
+++ src/lib/air-datepicker.js
@@ -5,16 +5,67 @@ import { isTouchDevice } from '../functions/isTouchDevice'
 
 const init = () => {
 
-    const calendars = document.querySelectorAll('.-calendar-')
+    // const calendars = document.querySelectorAll('.-calendar-')
 
-    calendars.forEach(item => {
+    // calendars.forEach(item => {
 
-        new AirDatepicker(item, {
+    //     new AirDatepicker(item, {
+    //         locale: localeRu,
+    //         isMobile: isTouchDevice(),
+    //         autoClose: true,
+    //         minDate: new Date(),
+    //         position: item.dataset.position || 'bottom left'
+
+    //     })
+
+    // })
+
+    const forms = document.querySelectorAll('.-form-')
+
+    forms.forEach(form => {
+
+        const calendarFirst = form.querySelector('.-calendar-first-')
+        const calendarLast = form.querySelector('.-calendar-last-')
+
+        let dpMin, dpMax
+
+        dpMin = new AirDatepicker(calendarFirst, {
+
+            onSelect({date}) {
+
+                dpMax.update({
+                    
+                    minDate: date
+                    
+                })
+
+            },
+
             locale: localeRu,
             isMobile: isTouchDevice(),
             autoClose: true,
             minDate: new Date(),
-            position: item.dataset.position || 'bottom left'
+            position: calendarFirst.dataset.position || 'bottom left'
+
+        })
+
+        dpMax = new AirDatepicker(calendarLast, {
+
+            onSelect({date}) {
+
+                dpMin.update({
+
+                    maxDate: date
+
+                })
+
+            },
+
+            locale: localeRu,
+            isMobile: isTouchDevice(),
+            autoClose: true,
+            minDate: new Date(),
+            position: calendarLast.dataset.position || 'bottom left'
 
         })
 
